06d821c59d0921b501c11ef8f44685e61d2c5cef
Axell B <56314049+AxellBoue@users.noreply.github.com>
2020-09-11 16:37:03 +0200

    transition ciel mieux

--- godotScene/Scripts/transition ciel.gd
+++ godotScene/Scripts/transition ciel.gd
@@ -1,20 +1,42 @@
 extends Node2D
 
 
-var compteur = 0
+var jour = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimationPlayer.connect("animation_finished",self, "fin_anim")
+	$Timer.connect("timeout", self, "nuit")
+	start(10)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):	
-	if Input.is_action_just_pressed("ciel"):
-		if compteur == 0:
-			$AnimationPlayer.play("nuitTombe")
-			compteur = 1
+	pass
+
+
+func start(day):
+	jour = day
+	$Timer.start()
+
+
+func nuit() :
+	$AnimationPlayer.play("nuitTombe")
+
+func fin_anim(anim):
+	if anim == "nuitTombe":
+		if jour == 7 || jour == 10 || jour == 11:
+			$AnimationPlayer.play("aubeRateLeReveil")
 		else :
 			$AnimationPlayer.play("aube")
-			compteur = 0
+	if anim == "aube":
+		emit_signal("transition_over", "transition_out")
+	if anim == "aubeRateLeReveil":
+		if jour == 7  :
+			emit_signal("transition_over", "transition_out")
+		elif jour == 10 || jour == 11:
+			# change day
+			jour += 1
+			start(jour)
+			
