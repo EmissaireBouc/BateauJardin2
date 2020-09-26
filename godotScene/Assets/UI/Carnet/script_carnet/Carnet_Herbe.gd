22451eb0ecabb67d1d2ba1611893a74981b14bc6
rl <nainkult@gmail.com>
2020-09-26 11:36:07 +0200

    Integration des illustrations Carnet + Inv

--- godotScene/Assets/UI/Carnet/script_carnet/Carnet_Herbe.gd
+++ godotScene/Assets/UI/Carnet/script_carnet/Carnet_Herbe.gd
@@ -4,8 +4,8 @@ func setup(nom, PV, XP, Description):
 	var pvText = get_node("PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/InformationHerbe/HBoxContainer/VBoxContainer/PV")
 	var xpText = get_node("PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/InformationHerbe/HBoxContainer/VBoxContainer/XP")
 	$PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/NomHerbe/HBoxContainer/Nom.text = nom
-	$PanelContainer/VBoxContainer/DescriptionHerbe/Description.text = Description
-	$PanelContainer/VBoxContainer/HBoxContainer/Illustration.texture = load("res://Assets/UI/Carnet/Illustration/Plantes/%s" %"illu_" + nom + ".png")
+	$PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/DescriptionHerbe/Description.text = Description
+	$PanelContainer/VBoxContainer/HBoxContainer/CadreIllu/Illustration.texture = load("res://Assets/UI/Carnet/Illustration/Plantes/%s" %"illu_" + nom + ".png")
 	if PV >= 0 && PV < 3 :
 		pvText.text = "Arrosage : Important"
 	if PV >= 3 && PV < 6 :
